734dc7efa92c8a63dfab4b4f095f7e591b93f01a
Shahid Islam <khanshahid9283@gmail.com>
2023-10-29 00:58:53 +0530

    practice pattern E

---
+++ Java/src/pattern/PatternE.java
@@ -0,0 +1,17 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class PatternE {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
